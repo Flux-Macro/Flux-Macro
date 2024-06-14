1cf48fac887720308e6d35bfce98f9f636b66f3f
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-06-14 18:30:16 -0400

    bingle bongle

--- docs/script.js
+++ docs/script.js
@@ -6,14 +6,15 @@ window.addEventListener("load",()=>{
     var r = gcd (w, h);
     var aspectW = w/r;
     var aspectH = h/r;
-    console.log(`got aspect ratio ${aspectW} : ${aspectH}`);
-    var is54=(aspectW==5&&aspectH==4);
-    var is169=(aspectW==16&&aspectH==9);
-    var is1610=(aspectW==16&&aspectH==10);
-    var is32=(aspectW==3&&aspectH==2);
-    var is43=(aspectW==4&&aspectH==3);
-    var is85=(aspectW==8&&aspectH==5);
-    if(!is54&&!is169&&!is1610&&!is32&&!is43&&!is85){
-        document.write("<h1>This device is unsupported</h1>")
-    }
+    // unpractical as shit
+    // console.log(`got aspect ratio ${aspectW} : ${aspectH}`);
+    // var is54=(aspectW==5&&aspectH==4);
+    // var is169=(aspectW==16&&aspectH==9);
+    // var is1610=(aspectW==16&&aspectH==10);
+    // var is32=(aspectW==3&&aspectH==2);
+    // var is43=(aspectW==4&&aspectH==3);
+    // var is85=(aspectW==8&&aspectH==5);
+    // if(!is54&&!is169&&!is1610&&!is32&&!is43&&!is85){
+    //     document.write("<h1>This device is unsupported</h1>")
+    // }
 });
