ba9976cd923ea2914ec18da3378b214ffaf7d773
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-06-15 21:40:45 -0400

    a lot of shit im proud tho

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,27 @@
 window.addEventListener("load",()=>{
-    console.log("loaded");
-    function gcd (a, b) {return (b == 0) ? a : gcd (b, a%b);}
-    var w = screen.width;
-    var h = screen.height;
-    var r = gcd (w, h);
-    var aspectW = w/r;
-    var aspectH = h/r;
-    // unpractical as shit
-    // console.log(`got aspect ratio ${aspectW} : ${aspectH}`);
-    // var is54=(aspectW==5&&aspectH==4);
-    // var is169=(aspectW==16&&aspectH==9);
-    // var is1610=(aspectW==16&&aspectH==10);
-    // var is32=(aspectW==3&&aspectH==2);
-    // var is43=(aspectW==4&&aspectH==3);
-    // var is85=(aspectW==8&&aspectH==5);
-    // if(!is54&&!is169&&!is1610&&!is32&&!is43&&!is85){
-    //     document.write("<h1>This device is unsupported</h1>")
-    // }
+    const checkres=()=>{
+        function gcd (a, b) {return (b == 0) ? a : gcd (b, a%b);}
+        var w = screen.width;
+        var h = screen.height;
+        var r = gcd (w, h);
+        var aspectW = w/r;
+        var aspectH = h/r;
+        // unpractical as shit
+        // console.log(`got aspect ratio ${aspectW} : ${aspectH}`);
+        // var is54=(aspectW==5&&aspectH==4);
+        // var is169=(aspectW==16&&aspectH==9);
+        // var is1610=(aspectW==16&&aspectH==10);
+        // var is32=(aspectW==3&&aspectH==2);
+        // var is43=(aspectW==4&&aspectH==3);
+        // var is85=(aspectW==8&&aspectH==5);
+        // if(!is54&&!is169&&!is1610&&!is32&&!is43&&!is85){
+        //     document.write("<h1>This device is unsupported</h1>")
+        // }
+    }
+    setTimeout(()=>{
+        document.getElementById("loading").setAttribute("style","opacity:0;pointer-events: none !important;");
+    },500)
+    checkres();
 });
 
 $(window).scroll(function() {
