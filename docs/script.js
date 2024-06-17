1d22800e8a676b5fb615fe2c7bf035684cf7db59
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-06-17 15:18:27 -0400

    added mobile support

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,5 @@
-window.addEventListener("load",()=>{
+disableScroll();
+window.onload=()=>{
     const checkres=()=>{
         function gcd (a, b) {return (b == 0) ? a : gcd (b, a%b);}
         var w = screen.width;
@@ -18,11 +19,14 @@ window.addEventListener("load",()=>{
         //     document.write("<h1>This device is unsupported</h1>")
         // }
     }
+    waitForElm("#loading");
     setTimeout(()=>{
         document.getElementById("loading").setAttribute("style","opacity:0;pointer-events: none !important;");
     },500)
+    // document.getElementById("loading").remove();
     checkres();
-});
+    enableScroll();
+};
 
 $(window).scroll(function() {
     if($(this).scrollTop() >= 100) {
@@ -31,3 +35,7 @@ $(window).scroll(function() {
         $("nav")[0].classList.remove("nav_active");
     }
 });
+document.getElementById("nav_logo").addEventListener("click",(e)=>{
+    e.preventDefault();
+    window.location.href = "index.html";
+})
