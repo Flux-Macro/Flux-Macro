836b92a0259f95f9136dea5641b3358cff4d6f2a
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-06-30 13:14:17 -0400

    aaaa

---
+++ Flux Macro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Flux_Macro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Flux Macro/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Flux_Macro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
